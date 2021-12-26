8f279c185d7e22fa6079d4708df2af61e163d1e6
new5558 <new17353@gmail.com>
2021-12-26 22:37:53 +0700

    test: add tet for getClassPeriod

--- workshop/frontend/src/workshop/unit/1-getClassPeriod/getClassPeriod.test.ts
+++ workshop/frontend/src/workshop/unit/1-getClassPeriod/getClassPeriod.test.ts
@@ -1,8 +1,20 @@
 // thinc-org/cugetreg-frontend
 
+import { getClassPeriod } from "./getClassPeriod"
+import { Class } from "./types"
+
 describe('getClassPeriod', () => {
-    it.todo('Write test when period is defined')
-    it.todo('Write test when period is not defined')
+    it('Should format class period correctly if period is defined in sectionClass', () => {
+        const mockSectionClass = { period: { start: '10', end: '12' } } as Class
+        const result = getClassPeriod(mockSectionClass)
+        expect(result).toMatch('10 - 12')
+    })
+
+    it('Should return empty string when period is not defined', () => {
+        const mockSectionClass = {} as Class
+        const result = getClassPeriod(mockSectionClass)
+        expect(result).toMatch('')
+    })
 })
 
 export { }
