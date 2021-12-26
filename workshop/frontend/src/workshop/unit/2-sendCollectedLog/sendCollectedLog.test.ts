d24f8724eb4e319e7fbf6f8670f4be0d32eb88ee
new5558 <new17353@gmail.com>
2021-12-26 23:01:25 +0700

    test: add some test for sendCollectedLog

--- workshop/frontend/src/workshop/unit/2-sendCollectedLog/sendCollectedLog.test.ts
+++ workshop/frontend/src/workshop/unit/2-sendCollectedLog/sendCollectedLog.test.ts
@@ -2,10 +2,36 @@
 
 describe('sendCollectedLog', () => {
         // Reminder: Since this is unit tets, you have to mock ./httpClient with jest
-        it.todo('Write test when data length is 0')
-        it.todo(`Write test when NODE_ENV is development
-            Hint: you have to muate process.env.NODE_ENV manually in test code
-    `)
+
+        const postSpy = jest.fn()
+        const DEFAULT_ENV = process.env
+        jest.doMock("./httpClient", () => ({ httpClient: { post: postSpy } }))
+
+        beforeEach(() => {
+                jest.resetModules()
+                jest.clearAllMocks()
+                process.env = { ...DEFAULT_ENV }
+        })
+
+        afterAll(() => {
+                process.env = DEFAULT_ENV
+        })
+
+        it('Should return undefined when data length is 0', async () => {
+                const { sendCollectedLog } = await import('./sendCollectedLog')
+                const mockData = [] as any
+                const result = sendCollectedLog(mockData)
+                expect(result).not.toBeDefined()
+        })
+        it('Should return undefined when NODE_ENV is development', async () => {
+                (process.env as any).NODE_ENV = 'development'
+                const { sendCollectedLog } = await import('./sendCollectedLog')
+                const mockData = ['test'] as any
+                const result = sendCollectedLog(mockData)
+                expect(postSpy).toBeCalledWith('/clientlogging', mockData)
+                expect(result).not.toBeDefined()
+        })
+
         it.todo('Write test for Axios call')
         it.todo(`Write test for Axios call and error
             Hint: jest.spyOn console error
