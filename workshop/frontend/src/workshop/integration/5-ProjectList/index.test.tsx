0d77350a97c9f85f5d4a31dfb7888faa25e1e992
new5558 <new17353@gmail.com>
2021-12-26 22:28:14 +0700

    feat: add workshop integration todolist for tester

--- workshop/frontend/src/workshop/integration/5-ProjectList/index.test.tsx
+++ workshop/frontend/src/workshop/integration/5-ProjectList/index.test.tsx
@@ -1,5 +1,10 @@
-describe('useProjectList', () => {
-    // This is integration so do not try to mock/stub any components if necessary
-})
+describe("useProjectList", () => {
+  // This is integration so do not try to mock/stub any components if necessary
+  it.todo("Should display projects correctly at initial state");
+  it.todo(
+    "Should show filter when click on filter button and hide filter when click again"
+  );
+  it.todo("Should display filtered projects when type on search textfield");
+});
 
-export {}
+export {};
